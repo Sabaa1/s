d1b7d7a989741d4ecaf688bb6f07e1a2cebd40bc
Sabaa1 <91894485+Sabaa1@users.noreply.github.com>
2021-10-04 17:49:30 +0400

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WindowsFormsApp6
+{
+    class User
+    { 
+        public int id;
+        public string username;
+        public string password;
+    }
+}
